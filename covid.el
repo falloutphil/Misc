a21c2b4dbc7997b57ac3f0e5786fdaa273be5ef5
falloutphil <phil@beadling.co.uk>
2020-08-21 11:41:41 +0100

    Allow optional population entry

--- covid.el
+++ covid.el
@@ -3,9 +3,9 @@
 ;;; Only works if you comment out the user-error in org-table--row-type.
 
 ;;; Code:
-(defun covid (country)
-   "Helper function to get covid details from COUNTRY."
-   (interactive "sCountry Name? ")
+(defun covid (country population)
+   "Helper function to get covid details from COUNTRY.  If POPULATION is non-zero this is used directly (eg to match ECDC numbers)."
+   (interactive "sCountry Name?  \nnPopulation or 0? ")
    (switch-to-buffer (format "%s covid" country))
    (org-mode)
    (url-handler-mode t)
@@ -16,11 +16,13 @@
    (org-table-convert-region (point-min) (point-max))
    (org-table-insert-hline)
    (goto-char (point-max))
-   (let ((population
-	  (cdr (assoc 'population
-		      (elt (json-read-file (format "https://restcountries.eu/rest/v2/name/%s?fields=population" country)) 0)))))
+   (let ((p
+	  (if (eq population 0) 
+	      (cdr (assoc 'population
+			  (elt (json-read-file (format "https://restcountries.eu/rest/v2/name/%s?fields=population" country)) 0)))
+	    population)))
      (insert (format "#+TBLFM: $9=if (@# > 15, ($6-@-14$6)*100000/%d, 0)::$10=if (@# > 8, ($6-@-7$6)*100000/%d, 0)::$11='(orgtbl-ascii-draw $10 0 100 20)"
-		     population population)))
+		     p p)))
    (org-ctrl-c-ctrl-c)) ;recalc
 
 
